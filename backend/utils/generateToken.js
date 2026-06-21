5668a3c865e332385487829d2f69a2a9739586e0
drib14 <jhondribramirez7@gmail.com>
2026-06-21 22:44:30 +0800

    refactor: remove automated patching scripts and auxiliary configuration files

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '7d',
-  });
-};
-
-export default generateToken;
